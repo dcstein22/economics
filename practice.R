02981b4b540374da793c73f1e22268bce2a0b061
Danny Stein <dcstein2@illinois.edu>
2021-05-03 16:22:13 -0500

    practicing using my own repository

---
+++ practice.R
@@ -0,0 +1,11 @@
+library(wooldridge)
+attach(hprice1)
+names(hprice1)
+h_price <- lm(price~lotsize+sqrft+bdrms)
+summary(h_price)
+res <- resid(h_price)
+sq <- res**2
+homosk <- lm(sq~lotsize+sqrft+bdrms)
+summary(homosk)
+pf(5.34,3,84,lower.tail=F)
+pchisq(14.09,3,lower.tail=F)
